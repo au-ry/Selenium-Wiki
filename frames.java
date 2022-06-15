b122f762a69e2d28a678c71ec5672d9c580a4bcc
au-ry <96151149+au-ry@users.noreply.github.com>
2022-06-15 08:31:19 -0400

    Create frames.java

---
+++ frames.java
@@ -0,0 +1,24 @@
+package apptestting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class frames {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        WebElement iframe = driver.findElement(By.cssSelector("//fieldset[contains(.,'iFrame Example')]"));
+
+        driver.switchTo().frame(iframe);
+
+
+        WebElement reg = driver.findElement(By.xpath("/html/body/div/header/div[2]/div/div/div[2]/div[1]/a"));
+        reg.click();
+    }
+}
+
