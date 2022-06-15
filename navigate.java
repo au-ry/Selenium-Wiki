e44d7b42167082a7e5bf84d247abf7b3d3ae84b9
au-ry <96151149+au-ry@users.noreply.github.com>
2022-06-15 11:54:42 -0400

    Update navigate.java

--- navigate.java
+++ navigate.java
@@ -4,17 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class exercise3 { //NAVIGATION COMMANDS
+public class navigation { //NAVIGATION COMMANDS
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
 
-        String pagina ="https://demoqa.com/";
-        driver.get(pagina);
+        String pagina ="https://the-internet.herokuapp.com/";
+        driver.navigate().to(pagina);
 
-        driver.findElement(By.className("card-up")).click();
+        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a")).click();
 
         driver.navigate().back();
         driver.navigate().forward();
